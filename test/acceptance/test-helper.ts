9165b1a19ddb44cc4d45d1c7774303bb5a838a7a
Matthew Hummer <biggreyson@gmail.com>
2019-01-25 21:42:53 -0700

    init app

---
+++ test/acceptance/test-helper.ts
@@ -0,0 +1,24 @@
+import {LoopbackDevApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const app = new LoopbackDevApplication({
+    rest: givenHttpServerConfig(),
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: LoopbackDevApplication;
+  client: Client;
+}
