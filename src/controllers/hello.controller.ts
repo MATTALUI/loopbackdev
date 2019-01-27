d02fc05c8102e482864625b51aa8a494a4063309
Matthew Hummer <biggreyson@gmail.com>
2019-01-26 23:24:56 -0700

    add todos demo

---
+++ src/controllers/hello.controller.ts
@@ -0,0 +1,18 @@
+// Uncomment these imports to begin using these cool features!
+
+// import {inject} from '@loopback/context';
+import {get} from '@loopback/rest'
+
+export class HelloController {
+  constructor() {}
+
+  @get('/hello')
+  hello(): string {
+    return "hello world!!!!!!!!!!"
+  }
+
+  @get('/cats')
+  cats(): object {
+    return {hello: "world"}
+  }
+}
---
+++ src/models/todo.model.ts
@@ -0,0 +1,34 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Todo extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    required: true,
+  })
+  id: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+  })
+  description?: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+    default: false,
+  })
+  isComplete: boolean;
+
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
